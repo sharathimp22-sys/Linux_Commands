2ca0ee8b8813965466e39c3de299498343495f1c
sharathimp22-sys <sharathimp22@gmail.com>
2026-03-13 14:12:03 -0400

    Create file_operations.sh

---
+++ basic_commands/file_operations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+touch file.txt        # Create a new file
+mkdir myfolder        # Create a new folder
+rm file.txt           # Delete a file
+rm -rf myfolder       # Delete a folder
+cp file.txt backup/   # Copy a file
+mv file.txt docs/     # Move or rename a file
