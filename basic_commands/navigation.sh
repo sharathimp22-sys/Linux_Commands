ecf4f53c3a9ca3dac7a81b085b7edaa9c925d9ee
sharathimp22-sys <sharathimp22@gmail.com>
2026-03-13 14:08:35 -0400

    Update navigation.sh

--- basic_commands/navigation.sh
+++ basic_commands/navigation.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+pwd          # Print current directory
+ls           # List files
+ls -al       # List all files with details
+cd foldername  # Enter a folder
+cd ..        # Go back one level
+cd ~         # Go to home directory
