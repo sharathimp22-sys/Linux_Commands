d4d823ac709f4cbbc6c9caf1f39a6c011864e7a6
sharathimp22-sys <sharathimp22@gmail.com>
2026-03-13 14:13:31 -0400

    Create permissions.sh

---
+++ basic_commands/permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod +x script.sh         # Make file executable
+chmod 755 script.sh        # Set read/write/execute
+chown user:group file.txt  # Change file owner
+ls -l                      # View file permissions
